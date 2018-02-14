0c226d2df640928d54a20ab0d8dc2ef815f0f249
eeu8cf <36476631+eeu8cf@users.noreply.github.com>
2018-02-14 12:35:30 +0000

    Create index.php
    this is a test u faggots

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+	<meta name="description" content="Professional Photography Services">
+		<meta name="keywords" content="wedding photographer, events photographer, JudeAndrewGabrielPhotography, Jude Andrew Gabriel, Photoraphy, photographs">
+		<meta name="robots" content="index, follow">
+		<link rel="canonical" href="http://pages.bangor.ac.uk/~eeu8cf/WebTech/Assignment/index.html" />
+		<meta http-equiv="refresh" content="600">
+        <meta charset="UTF-8">
+        <title>JAG Photography</title>
+		
+		<!-- CSS file -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+		
+		<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-86810408-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+		
+	</head>
+	
+    <body>
+	
+		
+		<header><!-- MENU BAR -->
+		<ul class="nav">
+			<li><a href="index.html">HOME</a></li>
+			<li><a href="gallery.html">GALLERY</a></li>
+			<li><a href="about_me.html">ABOUT ME</a></li>
+			<li><a href="contact_me.html">CONTACT ME</a></li>
+		</ul>
+			
+		</header>
+		<!-- BANNER -->
+		<div class="banner">
+			<img class="banner-img" src="Images/banner1.png" >
+		</div>	
+		<!-- SERVICES -->
+		 <div class="hmtitle">SERVICES</div>
+			<div align="center" class="services">
+				<div class="ServContent">
+				
+					<h1>WEDDING</h1>
+					<div class="popup"><img class="image" id="wedding1" src="Images/wedding.gif"></div>
+					<p> For your special and important moment in life, different angles and 
+					professional shots based on experience working with weddings. 
+					Various posses will be provided to give a beautiful unique wedding picture album.</p>
+				
+				</div>
+			<div class="ServContent">
+					<h1>CHRISTENING</h1>
+					<a href="#"><img class="image" src="Images/christening.gif"></a>
+					<p> Professional shots before, during and after christening of your special one.
+					Satisfaction guarenteed. </p>
+				
+				</div>
+				<div class="ServContent">
+					<h1>PARTY</h1>
+					<a href="#"><img class="image" src="Images/party.gif"></a>
+					<p> Are you hosting a party? Be it a simple reception party to a 
+					funky crazy party, all the special moments wont be missed out on camera.
+					Guest wont know when and how the special moment was shot.</p>
+				
+				</div>
+				<div class="ServContent">
+					<h1>ANY EVENT</h1>
+					<a href="#"><img class="image" src="Images/other.gif"></a>
+					<p> Any event which you need a good shot for either your advertisement
+					to professional family pictures, just give the date and time and I will be ready to click you in. </p>
+				
+				</div>
+			</div>
+			
+			<!-- popup -->
+			<div class="show">
+				<div class="overlay"></div>
+					<div class="img-show">
+						<span>X</span>
+						<img src="">
+					</div>
+			</div>
+			
+			<!-- Start Scripts -->
+			<script type="text/javascript" src="js/jquery.min.js"></script>
+			<script type="text/javascript" src="js/script.js"></script>
+			<!-- End Scripts -->
+		<footer>
+		<div class="Copyright">
+		<p>&copy; 2016 Copyright. All rights reserved</p>
+		</div>
+		</footer>
+	</body>
+</html>
+
+
